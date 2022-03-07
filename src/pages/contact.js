0c0ef53627879fd058f441817c62fd0e044e4c55
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2022-03-07 16:09:55 -0300

    feat(contact): font-awesome library imported and icons apllied

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,8 @@ import OpenAccount from '../components/OpenAccount';
 import { getLocations } from '../utils/queries';
 import client from '../utils/client';
 import { Container } from '../components/Guide';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 export default function Offices({ locations }) {
   return (
@@ -15,16 +17,23 @@ export default function Offices({ locations }) {
           <h1 className='font-bold text-6xl text-white'>Fale com a VOGA</h1>
         </div>
       </ContainerWithBackgroundImage>
-      <Container>
+      <Container className='py-48'>
         <div className='col-span-5'>
-          <p className='text-2xl'>
-            +55 61 3256-3080
-          </p>
-          <p className='text-2xl'>
-            contato@vogainvest.com
-          </p>
+          <div className='flex'>
+            <FontAwesomeIcon className='mr-4 pt-2' icon={faPhone} />
+            <p className='text-2xl'>
+              +55 61 3256-3080
+            </p>
+          </div>
+          <div className='flex mt-12'>
+            <FontAwesomeIcon className='mr-4 pt-2' icon={faEnvelope} />
+            <p className='text-2xl'>
+              contato@vogainvest.com
+            </p>
+          </div>
           {locations?.map(location => (
-            <div key={location.id}>
+            <div className='flex mt-12' key={location.id}>
+              <FontAwesomeIcon className='mr-4 pt-2' icon={faLocationDot} />
               <p className='text-2xl'>
                 <span className='font-bold text-3xl'>{location.title}: </span>{location.content_escritorio.summary}
               </p>
