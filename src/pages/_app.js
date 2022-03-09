b87321901b13c98a567e7fe8e462dcebc9222d6e
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2022-03-08 23:44:54 -0300

    fix(structure): removing styled components from the project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,11 @@
 import { ApolloProvider } from '@apollo/client';
 import client from '../utils/client';
-import GlobalStyles from '../styles/global';
 import '../styles/fonts.css';
 import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <GlobalStyles />
       <Component {...pageProps} />
     </ApolloProvider>
   );
